0defb0199ead230e43f88f66d35a2f6cd5fa5e4b
gdanko <gdanko@gmail.com>
2012-08-02 13:20:23 -0700

    Add "Disable boot animation" option to ROM Control > General UI
    Toggle the boot animation. Call the user a Jerk if disabled.
    
    Patch-set 2: Create an array of insults. Each time you disable
    the boot animation, one of the array of insults will be
    randomly selected and displayed.
    
    Patch-set 3: In the onCreate method I need to pull from
    the list of insults if the checkbox is checked.
    
    Patch-set 4: Remove a block of spaces.
    
    Change-Id: Ibda02ca8983286757d311b4fec090377847fd6eb
    Signed-off-by: gdanko <gdanko@gmail.com>
    
    Conflicts:
    
    	res/values/strings.xml
    	res/xml/prefs_ui.xml
    	src/com/aokp/romcontrol/fragments/UserInterface.java
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import java.util.Random;
+
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
@@ -22,6 +25,8 @@ import android.widget.EditText;
 
 import com.aokp.romcontrol.AOKPPreferenceFragment;
 import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.util.CMDProcessor;
+import com.aokp.romcontrol.util.Helpers;
 
 public class UserInterface extends AOKPPreferenceFragment {
 
@@ -30,17 +35,27 @@ public class UserInterface extends AOKPPreferenceFragment {
     private static final String PREF_ENABLE_VOLUME_OPTIONS = "enable_volume_options";
 
     CheckBoxPreference mEnableVolumeOptions;
+    CheckBoxPreference mDisableBootAnimation;
 
+    Random randomGenerator = new Random();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs_ui);
-
         mEnableVolumeOptions = (CheckBoxPreference) findPreference(PREF_ENABLE_VOLUME_OPTIONS);
         mEnableVolumeOptions.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.ENABLE_VOLUME_OPTIONS, 0) == 1);
+
+        mDisableBootAnimation = (CheckBoxPreference)findPreference("disable_bootanimation");
+        mDisableBootAnimation.setChecked(!new File("/system/media/bootanimation.zip").exists());
+        if (mDisableBootAnimation.isChecked()) {
+            Resources res = mContext.getResources();
+            String[] insults = res.getStringArray(R.array.disable_bootanimation_insults);
+            int randomInt = randomGenerator.nextInt(insults.length);
+            mDisableBootAnimation.setSummary(insults[randomInt]);
+        }
     }
 
     @Override
@@ -53,6 +68,26 @@ public class UserInterface extends AOKPPreferenceFragment {
                     Settings.System.ENABLE_VOLUME_OPTIONS, checked ? 1 : 0);
             return true;
             }
+        } else if (preference == mDisableBootAnimation) {
+            boolean checked = ((CheckBoxPreference) preference).isChecked();
+            if (checked) {
+                Helpers.getMount("rw");
+                new CMDProcessor().su
+                        .runWaitFor("mv /system/media/bootanimation.zip /system/media/bootanimation.unicorn");
+                Helpers.getMount("ro");
+                Resources res = mContext.getResources();
+                String[] insults = res.getStringArray(R.array.disable_bootanimation_insults);
+                int randomInt = randomGenerator.nextInt(insults.length);
+                preference.setSummary(insults[randomInt]);
+            } else {
+                Helpers.getMount("rw");
+                new CMDProcessor().su
+                        .runWaitFor("mv /system/media/bootanimation.unicorn /system/media/bootanimation.zip");
+                Helpers.getMount("ro");
+                preference.setSummary("");
+            }
+            return true;
+        }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
