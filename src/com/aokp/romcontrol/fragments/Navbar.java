ccae54fdf40ae90942f1eb7a8bfc582b3c7ca8a5
Stevespear426 <stephen.k.spear@gmail.com>
2012-09-23 21:25:09 -0400

    NavBar: Add screenshot to navbar
    Addes screenshot button and longpress option to navbar
    
    Thanks Kwes1020 for the sweet icon.
    
    Patch Set2: Fix opacity of screenshot icon
    Patch Set3: Rebase
    Patch Set4: OCD cleanup
    
    Change-Id: I593f693044b2bdada457ebe3ca6e7e1ab35c1137
    
    Conflicts:
    
    	res/values/arrays.xml
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/aokp/romcontrol/fragments/Navbar.java
+++ src/com/aokp/romcontrol/fragments/Navbar.java
@@ -657,6 +657,8 @@ public class Navbar extends AOKPPreferenceFragment implements
             } else if (uri.equals("**search**")) {
 
                 return getResources().getDrawable(R.drawable.ic_sysbar_search);
+            } else if (uri.equals("**screenshot**")) {
+                return getResources().getDrawable(R.drawable.ic_sysbar_screenshot);
             } else if (uri.equals("**menu**")) {
 
                 return getResources().getDrawable(R.drawable.ic_sysbar_menu_big);
@@ -702,6 +704,8 @@ public class Navbar extends AOKPPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_recents);
             else if (uri.equals("**search**"))
                 return getResources().getString(R.string.navbar_action_search);
+            else if (uri.equals("**screenshot**"))
+                return getResources().getString(R.string.navbar_action_screenshot);
             else if (uri.equals("**menu**"))
                 return getResources().getString(R.string.navbar_action_menu);
             else if (uri.equals("**ime**"))
