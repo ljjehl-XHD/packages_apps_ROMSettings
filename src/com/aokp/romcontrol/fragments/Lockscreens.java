b034e40cd85ce8fda25dc9a4afb468014c6f007a
Stevespear426 <stephen.k.spear@gmail.com>
2012-12-03 21:01:06 +0100

    Lockscreen allow homescreen widgets
    Big time warning to users about the possiblity of some widgits causing
    problems...
    
    Strings clean up
    
    Change-Id: I52a57cb02b58c30bab4759cf295b5c25836cead4

--- src/com/aokp/romcontrol/fragments/Lockscreens.java
+++ src/com/aokp/romcontrol/fragments/Lockscreens.java
@@ -68,6 +68,7 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
 
     private static final String PREF_QUICK_UNLOCK = "lockscreen_quick_unlock_control";
     private static final String PREF_LOCKSCREEN_AUTO_ROTATE = "lockscreen_auto_rotate";
+    private static final String PREF_LOCKSCREEN_ALL_WIDGETS = "lockscreen_all_widgets";
     private static final String PREF_LOCKSCREEN_BATTERY = "lockscreen_battery";
     private static final String PREF_LOCKSCREEN_TEXT_COLOR = "lockscreen_text_color";
 
@@ -83,6 +84,7 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
 
     CheckBoxPreference mQuickUnlock;
     CheckBoxPreference mLockscreenBattery;
+    CheckBoxPreference mLockscreenAllWidgets;
     ColorPickerPreference mLockscreenTextColor;
     CheckBoxPreference mLockscreenAutoRotate;
 
@@ -105,6 +107,10 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
         mLockscreenBattery.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
                 Settings.System.LOCKSCREEN_BATTERY, false));
 
+        mLockscreenAllWidgets = (CheckBoxPreference)findPreference(PREF_LOCKSCREEN_ALL_WIDGETS);
+        mLockscreenAllWidgets.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
+                Settings.System.LOCKSCREEN_ALL_WIDGETS, false));
+
         mLockscreenTextColor = (ColorPickerPreference) findPreference(PREF_LOCKSCREEN_TEXT_COLOR);
         mLockscreenTextColor.setOnPreferenceChangeListener(this);
 
@@ -124,6 +130,11 @@ public class Lockscreens extends AOKPPreferenceFragment implements OnPreferenceC
        if (preference == mQuickUnlock) {
             Settings.System.putBoolean(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
+                    ((CheckBoxPreference) preference).isChecked());
+            return true;
+        } else if (preference == mLockscreenAllWidgets) {
+            Settings.System.putBoolean(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_ALL_WIDGETS,
                     ((CheckBoxPreference) preference).isChecked());
             return true;
         } else if (preference == mLockscreenWallpaper) {
