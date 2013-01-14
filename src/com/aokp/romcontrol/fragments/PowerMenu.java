7b444b543699ff1ac7ffeb22ce4ff1e2ba611e34
iceandfire <arhamjamal@gmail.com>
2013-01-14 17:21:31 +0530

    Expanded desktop.
    RS part of expanded desktop.
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/aokp/romcontrol/fragments/PowerMenu.java
+++ src/com/aokp/romcontrol/fragments/PowerMenu.java
@@ -19,12 +19,14 @@ public class PowerMenu extends AOKPPreferenceFragment {
     //private static final String PREF_TORCH_TOGGLE = "show_torch_toggle";
     //private static final String PREF_AIRPLANE_TOGGLE = "show_airplane_toggle";
     private static final String PREF_NAVBAR_HIDE = "show_navbar_hide";
+    private static final String PREF_EXPANDED_DESKTOP = "power_menu_expanded_desktop";
 
     //CheckBoxPreference mShowPowerSaver;
     //CheckBoxPreference mShowScreenShot;
     //CheckBoxPreference mShowTorchToggle;
     //CheckBoxPreference mShowAirplaneToggle;
     CheckBoxPreference mShowNavBarHide;
+    CheckBoxPreference mExpandedDesktopPref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,6 +66,11 @@ public class PowerMenu extends AOKPPreferenceFragment {
         mShowNavBarHide.setChecked(Settings.System.getBoolean(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
                 false));
+                
+        mExpandedDesktopPref = (CheckBoxPreference) findPreference(PREF_EXPANDED_DESKTOP);
+        mExpandedDesktopPref.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 
+                1) == 1);
     }
 
     @Override
@@ -96,6 +103,11 @@ public class PowerMenu extends AOKPPreferenceFragment {
             Settings.System.putBoolean(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_NAVBAR_HIDE,
                     ((CheckBoxPreference)preference).isChecked());
+            return true;
+        } else if (preference == mExpandedDesktopPref) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED,
+                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
             return true;
         }
 
