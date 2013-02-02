12148df7539800f0d11595b09b247ff244452097
iceandfire <arhamjamal@gmail.com>
2013-02-02 11:23:22 +0530

    Add notification and chevron.
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -100,6 +100,9 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final String PIE_GRAVITY = "pie_gravity";
     private static final String PIE_MODE = "pie_mode";
     private static final String PIE_SIZE = "pie_size";
+    private static final String PIE_TRIGGER = "pie_trigger";
+    private static final String PIE_GAP = "pie_gap";
+    private static final String PIE_NOTIFICATIONS = "pie_notifications";
 
     private static final int REQUEST_PICK_WALLPAPER = 201;
     private static final int REQUEST_PICK_CUSTOM_ICON = 202;
@@ -130,6 +133,9 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     ListPreference mPieSize;
     ListPreference mPieGravity;
     CheckBoxPreference mPieControls;
+    ListPreference mPieTrigger;
+    ListPreference mPieGap;
+    CheckBoxPreference mPieNotifi;
 
     private AnimationDrawable mAnimationPart1;
     private AnimationDrawable mAnimationPart2;
@@ -249,6 +255,22 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
                 Settings.System.PIE_SIZE);
         mPieSize.setValue(pieSize != null && !pieSize.isEmpty() ? pieSize : "1");
         mPieSize.setOnPreferenceChangeListener(this);
+        
+        mPieTrigger = (ListPreference) findPreference(PIE_TRIGGER);
+        String pieTrigger = Settings.System.getString(mContext.getContentResolver(),
+                Settings.System.PIE_TRIGGER);
+        mPieTrigger.setValue(pieTrigger != null && !pieTrigger.isEmpty() ? pieTrigger : "1");
+        mPieTrigger.setOnPreferenceChangeListener(this);
+
+        mPieGap = (ListPreference) findPreference(PIE_GAP);
+        int pieGap = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_GAP, 1);
+        mPieGap.setValue(String.valueOf(pieGap));
+        mPieGap.setOnPreferenceChangeListener(this);
+
+        mPieNotifi = (CheckBoxPreference) findPreference(PIE_NOTIFICATIONS);
+        mPieNotifi.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.PIE_NOTIFICATIONS, 0) == 1));
                         
         mLcdDensity = findPreference("lcd_density_setup");
         String currentProperty = SystemProperties.get("ro.sf.lcd_density");
@@ -269,6 +291,9 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         mPieGravity.setEnabled(pieCheck);
         mPieMode.setEnabled(pieCheck);
         mPieSize.setEnabled(pieCheck);
+        mPieTrigger.setEnabled(pieCheck);
+        mPieGap.setEnabled(pieCheck);
+        mPieNotifi.setEnabled(pieCheck);
     }
 
     private void updateCustomLabelTextSummary() {
@@ -397,6 +422,11 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
                     Settings.System.PIE_CONTROLS,
                     mPieControls.isChecked() ? 1 : 0);
             checkControls();
+            Helpers.restartSystemUI();
+        } else if (preference == mPieNotifi) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.PIE_NOTIFICATIONS,
+                    mPieNotifi.isChecked() ? 1 : 0);
         }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -422,6 +452,16 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
             int pieGravity = Integer.valueOf((String) newValue);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.PIE_GRAVITY, pieGravity);
+            return true;
+        } else if (preference == mPieGap) {
+            int pieGap = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.PIE_GAP, pieGap);
+            return true;
+        } else if (preference == mPieTrigger) {
+            float pierigger = Float.valueOf((String) newValue);
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.PIE_TRIGGER, pierigger);
             return true;
         }
         return false;
