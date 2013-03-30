7ba2e7b9f58f3fc58c6bdec97997e8f4adfaaed1
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-03-30 22:34:07 +0100

    Toggles: Add screenshot delay option
    PS2: Move individual Toggle options to the bottom
    PS3: Swap screenshot/fav order
    
    Change-Id: I7c821eac230392c577051cfec47255175019701a
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/prefs_statusbar_toggles.xml
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/aokp/romcontrol/fragments/StatusBarToggles.java
+++ src/com/aokp/romcontrol/fragments/StatusBarToggles.java
@@ -43,6 +43,7 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
 	private static final String PREF_QUICK_THEME_STYLE = "quick_theme_style";
     private static final String PREF_ENABLE_FASTTOGGLE = "enable_fast_toggle";
     private static final String PREF_CHOOSE_FASTTOGGLE_SIDE = "choose_fast_toggle_side";
+    private static final String PREF_SCREENSHOT_DELAY = "screenshot_delay";
 
     private final int PICK_CONTACT = 1;
 
@@ -54,6 +55,7 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
 	ListPreference mThemeStyle;
     CheckBoxPreference mFastToggle;
     ListPreference mChooseFastToggleSide;
+    ListPreference mScreenshotDelay;
 
     BroadcastReceiver mReceiver;
     ArrayList<String> mToggles;
@@ -107,6 +109,11 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
         mChooseFastToggleSide.setOnPreferenceChangeListener(this);
         mChooseFastToggleSide.setValue(Settings.System.getInt(mContentRes,
                 Settings.System.CHOOSE_FASTTOGGLE_SIDE, 1) + "");
+
+        mScreenshotDelay = (ListPreference) findPreference(PREF_SCREENSHOT_DELAY);
+        mScreenshotDelay.setOnPreferenceChangeListener(this);
+        mScreenshotDelay.setValue(String.valueOf(Settings.System.getInt(mContentRes,
+                Settings.System.SCREENSHOT_TOGGLE_DELAY, 5000)));
 
         if (isSW600DPScreen(mContext) || isTablet(mContext)) {
             getPreferenceScreen().removePreference(mFastToggle);
@@ -175,6 +182,11 @@ public class StatusBarToggles extends AOKPPreferenceFragment implements
                     Settings.System.TOGGLES_STYLE, val);
             mTogglesStyle.setValue((String) newValue);
             Helpers.restartSystemUI();
+        } else if (preference == mScreenshotDelay) {
+            int val = Integer.parseInt((String) newValue);
+            Settings.System.putInt(mContentRes,
+                    Settings.System.SCREENSHOT_TOGGLE_DELAY, val);
+            mScreenshotDelay.setValue((String) newValue);
         } else if (preference == mFastToggle) {
             boolean val = (Boolean) newValue;
             Settings.System.putBoolean(mContentRes,
