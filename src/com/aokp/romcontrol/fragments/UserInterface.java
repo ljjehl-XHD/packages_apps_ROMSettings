AOKP/packages_apps_ROMControl

ROMControl: GoogleNow in Recents
>>> Framework By Roman

Change-Id: Ic38f0815c17ccd24341bdc186aef447626a97122

src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -85,6 +85,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final CharSequence PREF_VIBRATE_NOTIF_EXPAND = "vibrate_notif_expand";
     private static final CharSequence PREF_LONGPRESS_TO_KILL = "longpress_to_kill";
     private static final CharSequence PREF_RECENT_KILL_ALL = "recent_kill_all";
+    private static final CharSequence PREF_RECENT_GOOGLE_ASSIST = "recent_google_assist";
     private static final CharSequence PREF_RAM_USAGE_BAR = "ram_usage_bar";
     private static final CharSequence PREF_IME_SWITCHER = "ime_switcher";
     private static final CharSequence PREF_STATUSBAR_BRIGHTNESS = "statusbar_brightness_slider";
@@ -125,6 +126,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     CheckBoxPreference mVibrateOnExpand;
     CheckBoxPreference mLongPressToKill;
     CheckBoxPreference mRecentKillAll;
+    CheckBoxPreference mRecentGNow;
     CheckBoxPreference mRamBar;
     CheckBoxPreference mShowImeSwitcher;
     CheckBoxPreference mStatusbarSliderPreference;
@@ -220,6 +222,10 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         mRecentKillAll = (CheckBoxPreference) findPreference(PREF_RECENT_KILL_ALL);
         mRecentKillAll.setChecked(Settings.System.getBoolean(mContentResolver,
                 Settings.System.RECENT_KILL_ALL_BUTTON, false));
+
+        mRecentGnow = (CheckBoxPreference) findPreference(PREF_RECENT_GOOGLE_ASSIST);
+        mRecentGnow.setChecked(Settings.System.getBoolean(mContentResolver,
+                Settings.System.RECENT_GOOGLE_ASSIST, false));
 
         mRamBar = (CheckBoxPreference) findPreference(PREF_RAM_USAGE_BAR);
         mRamBar.setChecked(Settings.System.getBoolean(mContentResolver,
@@ -509,6 +515,11 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
             boolean checked = ((TwoStatePreference) preference).isChecked();
             Settings.System.putBoolean(mContentResolver,
                     Settings.System.RECENT_KILL_ALL_BUTTON, checked);
+            return true;
+        } else if (preference == mRecentGnow) {
+            boolean checked = ((TwoStatePreference) preference).isChecked();
+            Settings.System.putBoolean(mContentResolver,
+                    Settings.System.RECENT_GOOGLE_ASSIST, checked);
             return true;
         } else if (preference == mRamBar) {
             boolean checked = ((TwoStatePreference) preference).isChecked();

ljjehl-XHD/packages_apps_ROMSettings

Conflicts:
	res/values/strings.xml
	res/xml/prefs_ui.xml
	src/com/aokp/romcontrol/fragments/UserInterface.java

src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -122,6 +122,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final String PREF_NOTIFICATION_OPTIONS = "options";
     //private static final String STATUSBAR_HIDDEN = "statusbar_hidden";
     private static final String PREF_SEE_TRHOUGH = "see_through";
+    private static final CharSequence PREF_RECENT_GOOGLE_ASSIST = "recent_google_assist";
 
     private static final int REQUEST_PICK_WALLPAPER = 201;
     private static final int REQUEST_PICK_CUSTOM_ICON = 202;
@@ -144,6 +145,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     CheckBoxPreference mVibrateOnExpand;
     CheckBoxPreference mRecentKillAll;
 	Preference mRamBar;
+    CheckBoxPreference mRecentGoog;
 	CheckBoxPreference mShowImeSwitcher;
 	CheckBoxPreference mStatusbarSliderPreference;
 	CheckBoxPreference mHideExtras;
@@ -239,6 +241,10 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         mRecentKillAll = (CheckBoxPreference) findPreference(PREF_RECENT_KILL_ALL);
         mRecentKillAll.setChecked(Settings.System.getBoolean(cr,
                 Settings.System.RECENT_KILL_ALL_BUTTON, false));
+
+        mRecentGoog = (CheckBoxPreference) findPreference(PREF_RECENT_GOOGLE_ASSIST);
+        mRecentGoog.setChecked(Settings.System.getBoolean(mContentResolver,
+                Settings.System.RECENT_GOOGLE_ASSIST, false));
 
 		mShowActionOverflow = (CheckBoxPreference) findPreference(PREF_SHOW_OVERFLOW);
         mShowActionOverflow.setChecked(Settings.System.getBoolean(getActivity().
@@ -499,6 +505,11 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NOTIFICATION_SHOW_WIFI_SSID,
                     mShowWifiName.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mRecentGoog) {
+            boolean checked = ((TwoStatePreference) preference).isChecked();
+            Settings.System.putBoolean(mContentResolver,
+                    Settings.System.RECENT_GOOGLE_ASSIST, checked);
             return true;
         } else if (preference == mWakeUpWhenPluggedOrUnplugged) {
             Settings.System.putBoolean(getActivity().getContentResolver(),