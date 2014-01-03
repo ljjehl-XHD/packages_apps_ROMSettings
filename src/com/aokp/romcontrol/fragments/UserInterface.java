47a3c832cd8aecdfde4e7335551c4fd460421545
iceandfire <arhamjamal@gmail.com>
2014-01-03 20:40:25 +0530

    ElectronBeam: Add an alternative screen-off animation (2/2)
    Thanks to OmniROM
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -25,6 +25,8 @@ import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
 
+import static android.provider.Settings.System.SCREEN_OFF_ANIMATION;
+
 import java.util.List;
 
 import com.aokp.romcontrol.AOKPPreferenceFragment;
@@ -37,8 +39,10 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static ContentResolver mContentResolver;
 
 	private static final String PREF_STATUSBAR_BRIGHTNESS = "statusbar_enable_brightness_slider";
+    private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
 
 	CheckBoxPreference mStatusbarSliderPreference;
+    ListPreference mScreenOffAnimationPreference;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,17 +55,52 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver cr = mContext.getContentResolver();
         mContentResolver = getContentResolver();
-
+        ContentResolver resolver = getActivity().getContentResolver();
 
     	mStatusbarSliderPreference = (CheckBoxPreference) findPreference(PREF_STATUSBAR_BRIGHTNESS);
     	mStatusbarSliderPreference.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
             Settings.System.STATUSBAR_ENABLE_BRIGHTNESS_SLIDER, true));
 
+        mScreenOffAnimationPreference = (ListPreference) findPreference(KEY_SCREEN_OFF_ANIMATION);
+        final int currentAnimation = Settings.System.getInt(resolver, SCREEN_OFF_ANIMATION,
+                1 /* CRT-off */);
+        mScreenOffAnimationPreference.setValue(String.valueOf(currentAnimation));
+        mScreenOffAnimationPreference.setOnPreferenceChangeListener(this);
+        updateScreenOffAnimationPreferenceDescription(currentAnimation);
+
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mScreenOffAnimationPreference) {
+            int value = Integer.parseInt((String) objValue);
+            try {
+                Settings.System.putInt(getContentResolver(), SCREEN_OFF_ANIMATION, value);
+                updateScreenOffAnimationPreferenceDescription(value);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "could not persist screen-off animation setting", e);
+            }
+            return true;
+        }
 
-        return true;
+        return false;
+    }
+
+    private void updateScreenOffAnimationPreferenceDescription(int currentAnim) {
+        ListPreference preference = mScreenOffAnimationPreference;
+        String summary;
+        if (currentAnim < 0) {
+            // Unsupported value
+            summary = "";
+        } else {
+            final CharSequence[] entries = preference.getEntries();
+            final CharSequence[] values = preference.getEntryValues();
+            if (entries == null || entries.length == 0) {
+                summary = "";
+            } else {
+                summary = entries[currentAnim].toString();
+            }
+        }
+        preference.setSummary(summary);
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
