83e7cf2e4aa9655af3a0f1bee8ce79212b89c30c
kecinzer <kecinzer@gmail.com>
2014-01-10 13:58:03 +0100

    [2/2] Indicate charging in battery % indicator
    By changing color of % indicator
    
    Change-Id: I71ccbc5211b6187fb0c82c9415fcffd3bf59391b
    Signed-off-by: kecinzer <kecinzer@gmail.com>
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	res/layout/fragment_statusbar_settings.xml
    	res/values-cs/strings.xml
    	res/values/strings.xml

--- src/com/aokp/romcontrol/fragments/StatusBarBattery.java
+++ src/com/aokp/romcontrol/fragments/StatusBarBattery.java
@@ -2,14 +2,18 @@ package com.aokp.romcontrol.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.aokp.romcontrol.R;
-import com.aokp.romcontrol.AOKPPreferenceFragment;
+import com.aokp.romcontrol.settings.BaseSetting.OnSettingChangedListener;
+import com.aokp.romcontrol.settings.CheckboxSetting;
 
 
-public class StatusBarBattery extends AOKPPreferenceFragment {
+public class StatusBarBattery extends Fragment implements OnSettingChangedListener {
+
+	CheckboxSetting mBatteryIndicator, mBatteryIndicatorPlugged;
 
     public StatusBarBattery() {
 
@@ -18,6 +22,22 @@ public class StatusBarBattery extends AOKPPreferenceFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.xml.prefs_statusbar_battery, container, false);
 
+    mBatteryIndicator = (CheckboxSetting) v.findViewById(R.id.battery_percentage_indicator);
+    mBatteryIndicatorPlugged = (CheckboxSetting) v.findViewById(R.id.battery_percentage_indicator_plugged);
+
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBatteryIndicator.setOnSettingChangedListener(this);
+    }
+
+    @Override
+    public void onSettingChanged(String table, String key, String oldValue, String value) {
+        if ("aokp".equals(table)) {
+            mBatteryIndicatorPlugged.setVisibility(mBatteryIndicator.isChecked() ? View.VISIBLE : View.GONE);
+        }
     }
 }
