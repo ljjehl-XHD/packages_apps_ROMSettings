f459837dd3278e347c577008cd03287dafa7e6e5
iceandfire <arhamjamal@gmail.com>
2014-02-16 21:26:14 +0530

    Handle navbar heights correct on dpi changes + more (2/2)
    Change-Id: I7067b29a2125ab643efbffcc173b38b426a1b8d7
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- src/com/aokp/romcontrol/fragments/navbar/NavbarDimenSettings.java
+++ src/com/aokp/romcontrol/fragments/navbar/NavbarDimenSettings.java
@@ -21,6 +21,7 @@ import android.view.MenuItem;
 
 import com.aokp.romcontrol.AOKPPreferenceFragment;
 import com.aokp.romcontrol.R;
+import com.aokp.romcontrol.Utils;
 
 public class NavbarDimenSettings extends AOKPPreferenceFragment implements
         OnPreferenceChangeListener {
@@ -45,13 +46,20 @@ public class NavbarDimenSettings extends AOKPPreferenceFragment implements
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.navbar_dimen_settings);
 
+        PreferenceScreen prefSet = getPreferenceScreen();
+
         mNavigationBarHeight =
             (ListPreference) findPreference(PREF_NAVIGATION_BAR_HEIGHT);
         mNavigationBarHeight.setOnPreferenceChangeListener(this);
 
         mNavigationBarWidth =
             (ListPreference) findPreference(PREF_NAVIGATION_BAR_WIDTH);
-        mNavigationBarWidth.setOnPreferenceChangeListener(this);
+        if (!Utils.isPhone(getActivity())) {
+            prefSet.removePreference(mNavigationBarWidth);
+            mNavigationBarWidth = null;
+        } else {
+            mNavigationBarWidth.setOnPreferenceChangeListener(this);
+        }
 
         // Booleans to enable/disable nav bar
         // overriding overlays
@@ -64,9 +72,32 @@ public class NavbarDimenSettings extends AOKPPreferenceFragment implements
         mEnableNavigationBar.setOnPreferenceChangeListener(this);
 
         updateNavbarPreferences(enableNavigationBar);
-
+        updateDimensionValues();
         setHasOptionsMenu(true);
 
+    }
+
+    private void updateDimensionValues() {
+        int navigationBarHeight = Settings.System.getInt(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, -2);
+        if (navigationBarHeight == -2) {
+            navigationBarHeight = (int) (getResources().getDimension(
+                    com.android.internal.R.dimen.navigation_bar_height)
+                    / getResources().getDisplayMetrics().density);
+        }
+        mNavigationBarHeight.setValue(String.valueOf(navigationBarHeight));
+
+        if (mNavigationBarWidth == null) {
+            return;
+        }
+        int navigationBarWidth = Settings.System.getInt(getContentResolver(),
+                            Settings.System.NAVIGATION_BAR_WIDTH, -2);
+        if (navigationBarWidth == -2) {
+            navigationBarWidth = (int) (getResources().getDimension(
+                    com.android.internal.R.dimen.navigation_bar_width)
+                    / getResources().getDisplayMetrics().density);
+        }
+        mNavigationBarWidth.setValue(String.valueOf(navigationBarWidth));
     }
 
     // Enable/disbale nav bar
@@ -96,17 +127,12 @@ public class NavbarDimenSettings extends AOKPPreferenceFragment implements
         alertDialog.setMessage(R.string.navbar_dimensions_reset_message);
         alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                int height = mapChosenDpToPixels(48);
-                height = mapChosenDpToPixels(48);
-                Settings.System.putInt(getContentResolver(),
-                        Settings.System.NAVIGATION_BAR_HEIGHT,
-                        height);
-                height = mapChosenDpToPixels(42);
-                Settings.System.putInt(getContentResolver(),
-                        Settings.System.NAVIGATION_BAR_WIDTH,
-                        height);
-                mNavigationBarHeight.setValue("48");
-                mNavigationBarWidth.setValue("42");
+                Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT, -2);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_WIDTH, -2);
+
+                updateDimensionValues();
             }
         });
         alertDialog.setNegativeButton(R.string.cancel, null);
@@ -117,17 +143,15 @@ public class NavbarDimenSettings extends AOKPPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
 	if (preference == mNavigationBarWidth) {
             String newVal = (String) newValue;
-            int dp = Integer.parseInt(newVal);
-            int width = mapChosenDpToPixels(dp);
-            Settings.System.putInt(getContentResolver(), Settings.System.NAVIGATION_BAR_WIDTH,
-                    width);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_WIDTH,
+                    Integer.parseInt(newVal));
             return true;
         } else if (preference == mNavigationBarHeight) {
             String newVal = (String) newValue;
-            int dp = Integer.parseInt(newVal);
-            int height = mapChosenDpToPixels(dp);
-            Settings.System.putInt(getContentResolver(), Settings.System.NAVIGATION_BAR_HEIGHT,
-                    height);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT,
+                    Integer.parseInt(newVal));
             return true;
         } else if (preference == mEnableNavigationBar) {
             Settings.System.putInt(getActivity().getContentResolver(),
@@ -137,28 +161,6 @@ public class NavbarDimenSettings extends AOKPPreferenceFragment implements
             return true;
         }
         return false;
-    }
-
-    public int mapChosenDpToPixels(int dp) {
-        switch (dp) {
-            case 48:
-                return getResources().getDimensionPixelSize(R.dimen.navigation_bar_48);
-            case 44:
-                return getResources().getDimensionPixelSize(R.dimen.navigation_bar_44);
-            case 42:
-                return getResources().getDimensionPixelSize(R.dimen.navigation_bar_42);
-            case 40:
-                return getResources().getDimensionPixelSize(R.dimen.navigation_bar_40);
-            case 36:
-                return getResources().getDimensionPixelSize(R.dimen.navigation_bar_36);
-            case 30:
-                return getResources().getDimensionPixelSize(R.dimen.navigation_bar_30);
-            case 24:
-                return getResources().getDimensionPixelSize(R.dimen.navigation_bar_24);
-            case 0:
-                return 0;
-        }
-        return -1;
     }
 
     @Override
