0bbf4788b26d6401950cf0510d348eb1507f7e21
Peter Carenza <pcarenza@gmail.com>
2014-05-20 21:57:36 -0400

    ROMSettings: Combine headset plugged commits from Omnirom:
        [2/2] ROMSettings: opens music app when headset is plugged
        [2/2] ROMSettings: Headset plug: Disable if music is active
    
    Change-Id: Iff049527b9c92d73389b3b148376a09fd24b354e

--- src/com/aokp/romcontrol/fragments/Sound.java
+++ src/com/aokp/romcontrol/fragments/Sound.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -21,12 +23,17 @@ import com.aokp.romcontrol.AOKPPreferenceFragment;
 import com.aokp.romcontrol.R;
 import com.aokp.romcontrol.service.FlipService;
 import com.aokp.romcontrol.service.HeadphoneService;
+import com.aokp.romcontrol.widgets.AppSelectListPreference;
+
+import java.net.URISyntaxException;
 
 public class Sound extends AOKPPreferenceFragment
         implements OnPreferenceChangeListener {
 
     private static final String KEY_VOLUME_OVERLAY = "volume_overlay";
     private static final String PREF_HEADPHONES_PLUGGED_ACTION = "headphone_audio_mode";
+    private static final String KEY_HEADSET_PLUG = "headset_plug";
+    private static final String KEY_HEADSET_MUSIC_ACTIVE = "headset_plug_music_active";
     private static final String PREF_BT_CONNECTED_ACTION = "bt_audio_mode";
     private static final String PREF_FLIP_ACTION = "flip_mode";
     private static final String PREF_USER_TIMEOUT = "user_timeout";
@@ -43,6 +50,8 @@ public class Sound extends AOKPPreferenceFragment
     ListPreference mFlipScreenOff;
     ListPreference mPhoneSilent;
     ListPreference mAnnoyingNotifications;
+    private AppSelectListPreference mHeadsetPlug;
+    private CheckBoxPreference mHeadsetMusicActive;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,6 +87,11 @@ public class Sound extends AOKPPreferenceFragment
         mPhoneSilent = (ListPreference) findPreference(PREF_PHONE_RING_SILENCE);
         mPhoneSilent.setValue((prefs.getString(PREF_PHONE_RING_SILENCE, "0")));
         mPhoneSilent.setOnPreferenceChangeListener(this);
+
+        mHeadsetPlug = (AppSelectListPreference) findPreference(KEY_HEADSET_PLUG);
+        mHeadsetPlug.setOnPreferenceChangeListener(this);
+        mHeadsetMusicActive = (CheckBoxPreference) findPreference(KEY_HEADSET_MUSIC_ACTIVE);
+        updateHeadsetPlugSummary();
 
         if (!hasPhoneAbility(mContext)) {
             getPreferenceScreen().removePreference(mPhoneSilent);
@@ -141,6 +155,15 @@ public class Sound extends AOKPPreferenceFragment
                 toggleFlipService();
             }
             return true;
+        } else if (preference == mHeadsetPlug) {
+           String value = (String) newValue;
+           Settings.System.putString(getContentResolver(),
+                    Settings.System.HEADSET_PLUG_ENABLED, value);	
+           updateHeadsetPlugSummary();
+        } else if (preference == mHeadsetPlug) {
+           Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADSET_PLUG_MUSIC_ACTIVE, 
+                    mHeadsetMusicActive.isChecked()? 1:0);
         } else if (preference == mVolumeOverlay) {
             int value = Integer.valueOf((String) newValue);
             int index = mVolumeOverlay.findIndexOfValue((String) newValue);
@@ -149,5 +172,35 @@ public class Sound extends AOKPPreferenceFragment
             mVolumeOverlay.setSummary(mVolumeOverlay.getEntries()[index]);
         }
         return false;
+    }
+
+    private void updateHeadsetPlugSummary(){
+        final PackageManager packageManager = getPackageManager();
+
+        mHeadsetPlug.setSummary(getResources().getString(R.string.headset_plug_positive_title));
+        mHeadsetMusicActive.setEnabled(false);
+
+        String headSetPlugIntentUri = Settings.System.getString(getContentResolver(), Settings.System.HEADSET_PLUG_ENABLED);
+        if (headSetPlugIntentUri != null) {
+            if(headSetPlugIntentUri.equals(Settings.System.HEADSET_PLUG_SYSTEM_DEFAULT)) {
+                 mHeadsetPlug.setSummary(getResources().getString(R.string.headset_plug_neutral_summary));
+                 mHeadsetMusicActive.setEnabled(true);
+            } else {
+                Intent headSetPlugIntent = null;
+                try {
+                    headSetPlugIntent = Intent.parseUri(headSetPlugIntentUri, 0);
+                } catch (URISyntaxException e) {
+                    headSetPlugIntent = null;
+                }
+
+                if(headSetPlugIntent != null) {
+                    ResolveInfo info = packageManager.resolveActivity(headSetPlugIntent, 0);
+                    if (info != null) {
+                        mHeadsetPlug.setSummary(info.loadLabel(packageManager));
+                        mHeadsetMusicActive.setEnabled(true);
+                    }
+                }
+            }
+        }
     }
 }
